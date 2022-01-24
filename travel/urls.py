99fd37365879473c23d418b91b7f57c578025cff
drakehitch <filin140123@gmail.com>
2022-01-24 14:30:46 +0500

    inheritance

--- travel/urls.py
+++ travel/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from travel.views import home
+from travel.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
+    path('about/', about),
 ]
--- travel/views.py
+++ travel/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     name = "Drake Hitch"
     return render(request, "home.html", {"name": name})
+
+
+def about(request):
+    name = "About us"
+    return render(request, "about.html", {"name": name})
