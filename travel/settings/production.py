bbaa3a5f0e900bd4fb1c609b141f8a00629a468f
drakehitch <filin140123@gmail.com>
2022-02-11 18:00:12 +0500

    server upd hf2

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-PROJECT_ROOT = os.path.dirname(__file__)
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+STATIC_URL = '/static/'
+# PROJECT_ROOT = os.path.dirname(__file__)
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
